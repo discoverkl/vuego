cc6c8d2f4a55a16c98940d46dea9762cb71cdfb8
discoverkl <discoverkl@gmail.com>
2020-01-26 18:55:44 +0800

    add cnative example

---
+++ examples/cnative/main.go
@@ -0,0 +1,26 @@
+//go:generate go run github.com/markbates/pkger/cmd/pkger -o examples/cnative
+package main
+
+import (
+	"log"
+
+	"github.com/discoverkl/vuego/chrome"
+	"github.com/markbates/pkger"
+)
+
+//int add(int a, int b);
+import "C"
+
+func add(a, b int) int {
+	return int(C.add(C.int(a), C.int(b)))
+}
+
+func main() {
+	win, err := chrome.NewApp(pkger.Dir("/examples/native/fe/dist"), 200, 200, 800, 600)
+	// win, err := browser.NewPage(pkger.Dir("/examples/native/fe/dist"))
+	if err != nil {
+		log.Fatal(err)
+	}
+	win.Bind("add", add)
+	<-win.Done()
+}
