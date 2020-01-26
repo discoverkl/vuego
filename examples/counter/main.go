d6cfc514766f785043b545ce1b5d9f302135b83d
discoverkl <discoverkl@gmail.com>
2020-01-26 18:56:11 +0800

    add counter example

---
+++ examples/counter/main.go
@@ -0,0 +1,39 @@
+//go:generate go run github.com/markbates/pkger/cmd/pkger -o examples/counter
+package main
+
+import (
+	"log"
+	"sync"
+
+	"github.com/discoverkl/vuego"
+	"github.com/markbates/pkger"
+)
+
+type counter struct {
+	sync.Mutex
+	count int
+}
+
+func (c *counter) Add(n int) {
+	c.Lock()
+	defer c.Unlock()
+	c.count = c.count + n
+}
+
+func (c *counter) Value() int {
+	c.Lock()
+	defer c.Unlock()
+	return c.count
+}
+
+func main() {
+	c := &counter{}
+	vuego.Bind("counterAdd", c.Add)
+	vuego.Bind("counterValue", c.Value)
+
+	addr := ":8000"
+	log.Printf("listen on: %s", addr)
+	if err := vuego.ListenAndServe(addr, pkger.Dir("/examples/counter/fe/dist")); err != nil {
+		log.Fatal(err)
+	}
+}
